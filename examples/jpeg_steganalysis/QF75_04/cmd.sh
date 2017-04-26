70a4bf710d427845fc0229b53db2dfe6736da0b5
Guanshuo Xu <guanshuo.xu@gmail.com>
2017-04-26 15:50:12 -0400

    Add files via upload

---
+++ examples/jpeg_steganalysis/QF75_04/cmd.sh
@@ -0,0 +1,3 @@
+#!/usr/bin/env sh
+
+/home/forensics/caffe-research/IH17/steganalysis-jpeg-ih17/build/tools/caffe train --solver=solver.prototxt --gpu 0
---
+++ examples/jpeg_steganalysis/QF75_04/cmd_test.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/env sh
+
+/home/forensics/caffe-research/IH17/steganalysis-jpeg-ih17/build/tools/caffe test -model CNN_test.prototxt -weights inference90000.caffemodel -gpu 0 -iterations 200 -prob prob90000.txt
+/home/forensics/caffe-research/IH17/steganalysis-jpeg-ih17/build/tools/caffe test -model CNN_test.prototxt -weights inference85000.caffemodel -gpu 0 -iterations 200 -prob prob85000.txt
+/home/forensics/caffe-research/IH17/steganalysis-jpeg-ih17/build/tools/caffe test -model CNN_test.prototxt -weights inference80000.caffemodel -gpu 0 -iterations 200 -prob prob80000.txt
